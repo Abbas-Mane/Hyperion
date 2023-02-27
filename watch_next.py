71300630da0c3f4b2b83dc944d92419f5301625b
Abbas-Mane <125306268+Abbas-Mane@users.noreply.github.com>
2023-02-27 16:12:22 +0000

    Update watch_next.py
    made it a function that takes in description of a movie and a text file of movies with description then returns a movie name as a suggestion on what to watch next

--- watch_next.py
+++ watch_next.py
@@ -1,42 +1,46 @@
 import spacy
 nlp = spacy.load('en_core_web_md')
 
-# makes a dictionary with movie title as key and movie plot as value
-with open("movies.txt", "r", encoding="utf-8") as f:
-    content = f.readlines()
-    movies = {}
-    for line in content:
-        line = line.strip("\n").split(":")
-        movies[line[0]] = line[1]
+def watch_next(watched_movie_description, text_file):
+    '''
+    Returns movie name after comparing movie description provided against movie descriptions in text file
+    param watched_movie_description: variable containing description of movie watched already
+    :param text_file: text file containing movie names and descriptions from which function chooses most similar
+    :return: movie name from text file
+    '''
+    # Opens text file and creates 2 lists, 1 of movie names and 1 of movie descriptions
+    with open(text_file, "r", encoding="utf-8") as f:
+        content = f.readlines()
+        movie_names = []
+        movie_descriptions = []
+        for line in content:
+            line = line.strip("\n").split(":")
+            movie_names.append(line[0])
+            movie_descriptions.append(line[1])
+   # counter keeps track of which movie description most similar to already watched one
+    max_similarity = 0
+    best_match = ""
+    watched_movie_description = nlp(watched_movie_description)
+    # for loop checks each movie plot against the hulk one and finds similarity. If similarity is higher than previous movie
+    # then max_similarity is updated and movie plot is stored in variable best_match
+    for i in movie_descriptions:
+        similarity = nlp(i).similarity(watched_movie_description)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            best_match = i
+    #
+    position = movie_descriptions.index(best_match)
+    return movie_names[position]
 
-# makes 2 lists, 1 for dictionary keys and 1 for dictionary values. This will be used to get movie name from movie plot.
-movies_name = list(movies.keys())
-movies_plot = list(movies.values())
 
-movie_to_compare = '''Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati
-trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk 
-land on the planet Sakaar where he is sold into slavery and trained as a gladiator.'''
+text_file = "movies.txt"
+watched_movie_description = '''
+Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into
+a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet
+Sakaar where he is sold into slavery and trained as a gladiator.'''
 
-# keeps track of which plot has highest similarity to above movie plot
-max_similarity = 0
-best_match = ""
-model_movie = nlp(movie_to_compare)
 
-# for loop checks each movie plot against the hulk one and finds similarity. If similarity is higher than previous movie
-# then max_similarity is updated and movie plot is stored in variable best_match
-for movie_plot in movies_plot:
-    similarity = nlp(movie_plot).similarity(model_movie)
-    if similarity > max_similarity:
-        max_similarity = similarity
-        best_match = movie_plot
-
-# This is to test code is working. Outputs final values for max_similarity and closest movie plot
-print(max_similarity)
-print(best_match)
-
-# Index value of movie plot is same as index value of movie name. Prints out movie name key matching chosen movie plot
-position = movies_plot.index(best_match)
-print(f"The next movie to watch is {movies_name[position]}")
+print(f'The next movie to watch is {watch_next(watched_movie_description, "movies.txt")}')
 
 
 
